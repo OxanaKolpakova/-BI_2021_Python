aedb5e46aa3dbe5b6d3f74b3e8b4a4c6f5d48081
Oxana Kolpakova <90484234+OxanaKolpakova@users.noreply.github.com>
2021-11-25 21:18:10 +0300

    Update 5.py

--- regular_expression/5/5.py
+++ regular_expression/5/5.py
@@ -1,6 +1,6 @@
 import re
 import matplotlib.pyplot as plt
-import seaborn as sns
+# import seaborn as sns
 import pandas as pd
 wlist = []
 low_reg = []
@@ -10,7 +10,7 @@ with open('input.txt') as input:
     text = input.read()
 pattern = re.compile(r'\b[A-z]+\b')
 find = pattern.findall(text)
-#print(*find, sep='\n')
+# print(*find, sep='\n')
 for word in find:
     low_reg.append(word.lower())
 for word in low_reg:
@@ -21,9 +21,9 @@ for word in wlist:
     leng_list.append(len(word))
 for i in range(1, max(leng_list)+1):
     unic_list.append(leng_list.count(i))
-y=unic_list
-x=range(1,16)
-df = pd.DataFrame({'word length':x, 'count':y})
+y = unic_list
+x = range(1, 16)
+df = pd.DataFrame({'word length': x, 'count': y})
 ax = df.plot.bar(x='word length', y='count', rot=30)
 plt.ticklabel_format(style='plain', axis='y')
 plt.show()
