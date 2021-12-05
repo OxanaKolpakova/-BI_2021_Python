f3ca3a712cd7fc396a7b3fd1c44673078787734b
Oxana Kolpakova <90484234+OxanaKolpakova@users.noreply.github.com>
2021-12-05 17:56:14 +0300

    Update functional.py

--- functional.py
+++ functional.py
@@ -5,19 +5,19 @@ def sequential_map(seq, *args):
 
 
 def consensus_filter(seq, *args):
-  list = []
-  for i in seq:
-    check = 1
-    for func in args:
-      check = check*func(i)
-    if check == 1:
-      list.append(i)
-  return(list)
+      list = []
+      for i in seq:
+        check = 1
+        for func in args:
+            check = check*func(i)
+        if check == 1:
+            list.append(i)
+      return(list)
 
 
 def conditional_reduce(seq, func_1, func_2):
-  sum = 0
-  for i in seq:
-    if func_1(i):
-      sum = func_2(sum, i)
-  return(sum)
+      sum = 0
+      for i in seq:
+        if func_1(i):
+            sum = func_2(sum, i)
+      return(sum)
