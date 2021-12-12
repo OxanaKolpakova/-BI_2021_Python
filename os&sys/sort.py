22cbc1dfd0979d425c33ce8aa9ad3381ab9fabe0
Oxana Kolpakova <90484234+OxanaKolpakova@users.noreply.github.com>
2021-12-12 15:56:59 +0300

    Update sort.py

--- os&sys/sort.py
+++ os&sys/sort.py
@@ -6,7 +6,7 @@ import argparse
 filenames = sys.argv[1:]
 all_list = []
 for filename in filenames:
-	with open(filename) as file:
-		for line in file:
-			all_list.append(line)
+    with open(filename) as file:
+        for line in file:
+            all_list.append(line)
 print(*sorted(all_list))
