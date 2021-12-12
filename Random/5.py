420cd9765e1621d3f3a1e536baec35f028a8caf0
Oxana Kolpakova <90484234+OxanaKolpakova@users.noreply.github.com>
2021-12-12 15:36:56 +0300

    Update 5.py

--- Random/5.py
+++ Random/5.py
@@ -1,7 +1,7 @@
 import random
 
 
-def main(str): # перестановка букв
+def main(str):  # перестановка букв
     new_word = []
     new_str = ""
     for word in str.split():
@@ -9,13 +9,13 @@ def main(str): # перестановка букв
         suf = word[-1]
         middle = word[1:-1]
         middle = "".join(random.sample(middle, k=len(middle)))
-        if len(word)==1:
+        if len(word) == 1:
             new_word = word + " "
         else:
             new_word = pref + middle + suf+" "
         new_str = new_str + new_word
     return(new_str)
-  
+
 
 str = "П результатам исследования"
 print(main(str))
