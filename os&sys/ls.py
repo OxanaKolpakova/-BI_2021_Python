a4a51681c1b3c0f1693dbf383cd586f11b6b9548
Oxana Kolpakpva <oxkolpakova@gmail.com>
2021-12-12 15:44:33 +0300

    os&sys

---
+++ os&sys/ls.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+ 
+import sys
+import argparse
+import os
+if len(sys.argv)==1:
+	print(*os.listdir("."), sep="\n")
+else:
+	dirs = sys.argv[1:]
+	for dir in dirs:
+		print(dir, end = "" )
+		print(":")
+		print(*os.listdir(dir), sep="\n")
