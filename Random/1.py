f96d7ceca7c8f66f4e5b16fed503762d54b3cdf2
Oxana Kolpakova <90484234+OxanaKolpakova@users.noreply.github.com>
2021-12-12 15:34:12 +0300

    Update 1.py

--- Random/1.py
+++ Random/1.py
@@ -11,7 +11,7 @@ def Random_Plot(number=100):
         for j in range(number-i, number):
             random.random()
         time_list.append(time.time() - start_time)
-    #print(time_list)
+    # print(time_list)
     plt.plot(range(number), time_list)
     plt.title("Random.Random")
     plt.xlabel("number")
@@ -23,14 +23,14 @@ def NP_Random_Plot(number=100):
     time_list = []
     for i in range(number):
         start_time = time.time()
-        np.random.uniform(0,1,i)
+        np.random.uniform(0, 1, i)
         time_list.append(time.time() - start_time)
-    #print(time_list)
+    # print(time_list)
     plt.plot(range(number), time_list)
     plt.title("Numpy.Random")
     plt.xlabel("number")
     plt.ylabel("time")
     plt.show()
 
-NP_Random_Plot()
 
+NP_Random_Plot()
