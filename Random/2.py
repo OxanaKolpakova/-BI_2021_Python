dbbd5191a499eea13addcc72901fe4759cc53cb3
Oxana Kolpakova <90484234+OxanaKolpakova@users.noreply.github.com>
2021-12-12 15:23:36 +0300

    Update 2.py

--- Random/2.py
+++ Random/2.py
@@ -6,28 +6,27 @@ import numpy as np
 
 def sort_check(list):
     for i in range(len(list)-1):
-      if list[i]>list[i+1]:
-        return False
+        if list[i] > list[i+1]:
+            return False
     return True
 
 
 def monkey_sort(num_list):
-    while sort_check(num_list)==False:
-      random.shuffle(num_list)
+    while sort_check(num_list) == False:
+        random.shuffle(num_list)
     return(num_list)
-  
-  
-def main(number = 10, iter = 5): # max number of list and iterations
+
+
+def main(number=10, iter=5): # max number of list and iterations
     time_list = []
     mean_time = []
     var_time = []
-    a=0
     for i in range(number):   # range of array
         for j in range(iter):        # times
-          start_time = time.time()
-          list = np.random.randint(0,100,i)
-          monkey_sort(list)
-          time_list.append(time.time() - start_time)
+              start_time = time.time()
+              list = np.random.randint(0,100,i)
+              monkey_sort(list)
+              time_list.append(time.time() - start_time)
         mean_time.append(np.mean(time_list))
         var_time.append(np.var(time_list))
     plt.plot(range(number), mean_time, label="mean")
