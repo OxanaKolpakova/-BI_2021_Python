2d335c790699a6f5e7ab4c8cf0a40cd141608bc9
Oxana Kolpakova <90484234+OxanaKolpakova@users.noreply.github.com>
2021-12-12 15:38:08 +0300

    Update 3.py

--- Random/3.py
+++ Random/3.py
@@ -11,13 +11,13 @@ def random_walk(num_steps=5000):
   coordinate = [0,0]
   step_list = [[0,1],[0,-1],[1,0],[-1,0]]
   for num in range(num_steps):
-    step = random.choice(step_list)
-    coordinate = [sum(i) for i in zip_longest(coordinate, step, fillvalue=0)]
-    coordinate_list.append(coordinate)
+      step = random.choice(step_list)
+      coordinate = [sum(i) for i in zip_longest(coordinate, step, fillvalue=0)]
+      coordinate_list.append(coordinate)
   #print(coordinate_list)
   for j in range(len(coordinate_list)):
-    x.append(coordinate_list[j][0])
-    y.append(coordinate_list[j][1])
+      x.append(coordinate_list[j][0])
+      y.append(coordinate_list[j][1])
   fig, ax = plt.subplots()
   ax.scatter(x = x, y = y)
   plt.xlabel("x")
