b832ad04ae38acb98a51d0fd3ffaab4ce7948af5
Oxana Kolpakova <90484234+OxanaKolpakova@users.noreply.github.com>
2021-12-12 15:56:07 +0300

    Update ls.py

--- os&sys/ls.py
+++ os&sys/ls.py
@@ -2,11 +2,11 @@
 
 import sys
 import os
-if len(sys.argv)==1:
+if len(sys.argv) == 1:
     print(*os.listdir("."), sep="\n")
 else:
     dirs = sys.argv[1:]
     for dir in dirs:
-        print(dir, end = "" )
+        print(dir, end="")
         print(":")
         print(*os.listdir(dir), sep="\n")
