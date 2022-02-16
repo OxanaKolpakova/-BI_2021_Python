d83fffa1b44d5e60a48b8b96d5deb513dea1decc
Oxana Kolpakova <90484234+OxanaKolpakova@users.noreply.github.com>
2022-02-16 22:00:16 +0300

    Update 1.py

--- classes/1.py
+++ classes/1.py
@@ -1,5 +1,4 @@
 class Fruit(object):
-
     def __init__(self, color, taste, mellowness):
         self.color = color
         self.taste = taste
@@ -7,14 +6,15 @@ class Fruit(object):
 
     def test(self):
         if 5 < self.mellowness <= 10:
-            return("Ready to be eaten")
+            return "Ready to be eaten"
         if self.mellowness > 10:
-            return("I'm rotten")
+            return "I'm rotten"
         else:
-            return("Wait")
+            return "Wait"
 
     def after_a_while(self):
         self.mellowness += 1
+
 
 if __name__ == "__main__":
     apple = Fruit("red", "sweet", 10)
