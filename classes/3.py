15d7029767616cd9bbe25577e8cf5ce41ae6f60b
Oxana Kolpakova <90484234+OxanaKolpakova@users.noreply.github.com>
2022-02-16 21:50:14 +0300

    Update 3.py

--- classes/3.py
+++ classes/3.py
@@ -2,6 +2,6 @@ class my_class(list):
 
     def add(self, *a):
         for i in a:
-           if i > 0:
-             self.append(i)
+            if i > 0:
+                self.append(i)
         return(self)
